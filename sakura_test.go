bc733275dbffd117f3b27ec48a4b092a1ed24c79
antark <ants.arks@gmail.com>
2016-04-24 17:04:09 +0800

    更新 test 文件

--- sakura_test.go
+++ sakura_test.go
@@ -6,6 +6,6 @@ import (
 )
 
 func TestSakura(t *testing.T) {
-	var s = "1+2; 3*4;"
+	var s = "1+2; 3*4; -(10+2);(1+2)*3+4*5+6<<2; 3.1416926*1.618; +3*-4; +\"3.1415\"*2.0; let a=10; ^10;"
 	Run(strings.NewReader(s))
 }
